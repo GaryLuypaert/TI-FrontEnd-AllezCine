cb9fd1e51d1565f6f6dfb3179b06ce8f1b33d156
Gary Luypaert <garyluypaert@MBP-de-Gary.home>
2017-09-07 00:32:49 +0200

    Grosse MAJ des vacances

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-
-  $(window).on('load',function(){
+/*
+  $(window).on('load',function(){                 A REACTIVER APRES CORRECTION
       $('#myModalAge').modal('show');
   });
-
+*/
 
   $(".categories-movie-series").mouseenter(function() {
     $(this).css("background-color", "#FF8D1B");
@@ -38,5 +38,31 @@ $(document).ready(function() {
     $(".moreSeriesBox").slideToggle("slow");
     $(".moreSeriesBtn").show();
   });
+
+  $('.link-menu').on('click', function(evt){
+   evt.preventDefault();
+var target = $(this).attr('href');
+$('html, body')
+   .stop()
+   .animate({scrollTop: $(target).offset().top}, 1000 );
+});
+
+
+      $("#connect-btn").click(function() {
+
+        // e.preventDefault();
+        var login = $('input[name=login-member]').val();
+        var password = $('#pass-member-id').val();
+        $.ajax({
+          url: "connection.php?login=" + login + "&password=" + password,
+          type: "POST",
+          contentType: 'application/x-www-form-urlencoded',
+          // data: "login=" + login + "&password=" + password,
+          dataType: "html"
+        })
+      });
+
+
+
 
 });
